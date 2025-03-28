c648806b4724253fcf793b166d18abf2d7405e3c
Steven Nguyen <steven.n.1618@gmail.com>
2025-03-28 17:20:29 +1100

    revert example back

--- scripts/run_example.py
+++ scripts/run_example.py
@@ -3,16 +3,7 @@ from docker import APIClient
 
 docker_client = APIClient()
 runner = ModelRunner(
-    "examples/csiro.operators.snap-model-at-polygon-0.0.1",
+    "examples/simple",
     docker_client,
 )
-g = "POLYGON ((138.624287 -33.325508, 138.665657 -33.325508, 138.665657 -33.294521, 138.624287 -33.294521, 138.624287 -33.325508))"
-night_of = "2024-09-17"
-t = 1
-
-runner.run_model(docs={"input_geom":g,
-                       "input_Night_of":night_of,
-                       "input_threshold":t,
-                       "config": {'anything': 'e'},
-                       "secrets": {"id": "OS6QNMANYGL55VNL3DNZELHU",
-                                   "secret": "c6T9spGQtl3HfKrd0g0SeeM7HJBFqdmWT7N3FhrSke0="}})
+runner.run_model(docs={"input0": "1", "input1": "2"})
